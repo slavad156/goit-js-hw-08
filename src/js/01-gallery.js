ead7cbfcc264fff8d9f5115526c6c4aa29b431db
Viacheslav Diukov <slavad156@ukr.net>
2022-01-30 17:43:06 +0200

    finishes task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `  
+      <a class="gallery__item" href="${original}">
+        <img
+         class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+        />
+      </a>  
+    `;
+    })
+    .join('');
+}
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.innerHTML = galleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  navText: ['⇦', '⇨'],
+});
 
 console.log(galleryItems);
