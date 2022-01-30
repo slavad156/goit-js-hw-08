629928f41f4ff989173a54c33ca0d640488238c1
Viacheslav Diukov <slavad156@ukr.net>
2022-01-30 19:34:50 +0200

    finishes task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
